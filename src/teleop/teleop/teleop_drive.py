2f1fab6609cb5b6da99593749a884ada3e591e50
Leo Pan <leo.long.pan@gmail.com>
2026-03-19 17:51:54 -0900

    bruteforce can method

--- src/teleop/teleop/teleop_drive.py
+++ src/teleop/teleop/teleop_drive.py
@@ -11,10 +11,23 @@ class TeleopDrive(Node):
     def __init__(self):
         super().__init__('teleop_drive')
 
-        self.speedlimit = 3000 # RPM
+        self.BACK_RIGHT_INNER_ID = Vesc.id_conversion(13, 3)
+        self.BACK_RIGHT_OUTER_ID = Vesc.id_conversion(14, 3)
+        self.BACK_LEFT_INNER_ID = Vesc.id_conversion(15, 3)
+        self.BACK_LEFT_OUTER_ID = Vesc.id_conversion(16, 3)
+        self.FRONT_LEFT_INNER_ID = Vesc.id_conversion(17, 3)
+        self.FRONT_LEFT_OUTER_ID = Vesc.id_conversion(18, 3)
+        self.FRONT_RIGHT_INNER_ID = Vesc.id_conversion(19, 3)
+        self.FRONT_RIGHT_OUTER_ID = Vesc.id_conversion(20, 3)
+
+        self.left_speed = 0
+        self.right_speed = 0
+
+        self.speedlimit = 8000 # RPM
 
         self.dt_left_subscriber = self.create_subscription(Int8, 'dt_left', self.dt_left_callback, 10)
         self.dt_right_subscriber = self.create_subscription(Int8, 'dt_right', self.dt_right_callback, 10)
+        self.dt_can_publish_timer = self.create_timer(0.01, self.can_publish_timer_callback)
 
         try:
             self.bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate='500000')
@@ -35,28 +48,72 @@ class TeleopDrive(Node):
 
     def dt_left_callback(self, msg):
         self.get_logger().info('left data: "%s"' % msg.data)
-        rpm = int(msg.data / 100 * self.speedlimit)
-        self.get_logger().info('left rpm: "%s"' % rpm)
-        self.can_publish(
-            self.bus,
-            Vesc.id_conversion(15, 3),
-            Vesc.signal_conversion(rpm, 4),
-            True,
-        )
+        self.left_speed = int(msg.data / 100 * self.speedlimit)
+        # self.get_logger().info('left rpm: "%s"' % rpm)
 
 
     def dt_right_callback(self, msg):
         self.get_logger().info('right data: "%s"' % msg.data)
-        rpm = int(msg.data / 100 * self.speedlimit)
-        self.get_logger().info('right rpm: "%s"' % rpm)
+        self.right_speed = int(msg.data / 100 * self.speedlimit)
+        # self.get_logger().info('right rpm: "%s"' % rpm)
+        
+    
+    def can_publish_timer_callback(self):
+        # convert into a 4 byte array [255, 255, 255, 255]
+        left_rpm = Vesc.signal_conversion(self.left_speed, 4)
+        right_rpm = Vesc.signal_conversion(self.right_speed, 4)
+        
+        # publish left motors
         self.can_publish(
             self.bus,
-            Vesc.id_conversion(17, 3),
-            Vesc.signal_conversion(rpm, 4),
+            self.BACK_LEFT_INNER_ID,
+            left_rpm,
+            True,
+        )
+        self.can_publish(
+            self.bus,
+            self.BACK_LEFT_OUTER_ID,
+            left_rpm,
+            True,
+        )
+        self.can_publish(
+            self.bus,
+            self.FRONT_LEFT_INNER_ID,
+            left_rpm,
+            True,
+        )
+        self.can_publish(
+            self.bus,
+            self.FRONT_LEFT_OUTER_ID,
+            left_rpm,
             True,
         )
 
-    
+        # publish right motors
+        self.can_publish(
+            self.bus,
+            self.BACK_RIGHT_INNER_ID,
+            right_rpm,
+            True,
+        )
+        self.can_publish(
+            self.bus,
+            self.BACK_RIGHT_OUTER_ID,
+            right_rpm,
+            True,
+        )
+        self.can_publish(
+            self.bus,
+            self.FRONT_RIGHT_INNER_ID,
+            right_rpm,
+            True,
+        )
+        self.can_publish(
+            self.bus,
+            self.FRONT_RIGHT_OUTER_ID,
+            right_rpm,
+            True,
+        )
 
 def main(args=None):
     rclpy.init(args=args)
